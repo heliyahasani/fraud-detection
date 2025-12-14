8660a35f933a5860d3c2fcd0b26f7f0d67041dc3
Heliya Hasani <heliyahasani@hotmail.com>
2025-12-14 19:48:47 +0100

    export package modules in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,17 @@
 """Fraud detection package."""
+
+from .data import DataLoader
+from .evaluate import Evaluator, Metrics
+from .features import FeatureConfig, FeatureEngineer
+from .train import ModelType, TrainConfig, Trainer
+
+__all__ = [
+    "DataLoader",
+    "Evaluator",
+    "FeatureConfig",
+    "FeatureEngineer",
+    "Metrics",
+    "ModelType",
+    "TrainConfig",
+    "Trainer",
+]
